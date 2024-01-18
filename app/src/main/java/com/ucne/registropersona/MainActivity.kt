aad104ff3db3d5e7fc1260c99d72f4ba4a68a9cb
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2024-01-18 15:44:48 -0400

    Agrengando todos los campos del registro de persona.

--- app/src/main/java/com/ucne/registropersona/MainActivity.kt
+++ app/src/main/java/com/ucne/registropersona/MainActivity.kt
@@ -1,21 +1,37 @@
 package com.ucne.registropersona
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.ucne.registropersona.ui.theme.RegistroPersonaTheme
@@ -37,63 +53,136 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Registro(modifier: Modifier = Modifier){
-    Text(
-        text = "Registro de personas",
-        modifier = modifier
-            .padding(10.dp)
-    )
-    OutlinedTextField(value = "",
-        onValueChange = {},
-        placeholder ={
-            Text(text = "Nombres")
-        },
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 45.dp, horizontal = 8.dp)
-    )
-    OutlinedTextField(value = "",
-        onValueChange = {},
-        placeholder ={
-            Text(text = "Telefono")
-        },
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 110.dp, horizontal = 8.dp)
-    )
-    OutlinedTextField(value = "",
-        onValueChange = {},
-        placeholder ={
-            Text(text = "Celular")
-        },
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 175.dp, horizontal = 8.dp)
-    )
-    OutlinedTextField(value = "",
-        onValueChange = {},
-        placeholder ={
-            Text(text = "Email")
-        },
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 240.dp, horizontal = 8.dp)
-    )
-    OutlinedTextField(value = "",
-        onValueChange = {},
-        placeholder ={
-            Text(text = "Dirección")
-        },
-        modifier = modifier
-            .fillMaxWidth()
-            .padding(vertical = 305.dp, horizontal = 8.dp)
-    )
+fun CamposRegistro(modifier: Modifier = Modifier) {
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+    ) {
+        item {
+            Text(
+                text = "Registro de personas",
+                style = LocalTextStyle.current.copy(
+                    fontWeight = FontWeight.SemiBold
+                ),
+                modifier = modifier
+                    .padding(10.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Nombres")
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Telefono")
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Celular")
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Email")
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Text(text = "Dirección")
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            OutlinedTextField(
+                value = "",
+                onValueChange = {},
+                placeholder = {
+                    Row {
+                        Icon(
+                            imageVector = Icons.Default.DateRange,
+                            contentDescription = null
+                        )
+                        Text(text = " Fecha de nacimiento")
+                    }
+                },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+            )
+            val ocupaciones = listOf("Abogado", "Ingeniero", "Influencer")
+            var ocupacionSelecionada by remember { mutableStateOf("") }
+            var expandido by remember { mutableStateOf("") }
+            Column(modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp)
+            ) {
+                OutlinedTextField(
+                    leadingIcon = {
+                        Icon(
+                            imageVector = Icons.Default.ArrowDropDown,
+                            contentDescription = null
+                        )
+                    },
+                    label = { Text(text = "Selecciona ocupación") },
+                    value = ocupacionSelecionada,
+                    onValueChange = { ocupacionSelecionada = it },
+                    readOnly = true, enabled = false,
+                    modifier = Modifier
+                        .clickable {
+                            expandido = true.toString()
+                            Log.e("tag", "expandido")
+                        }
+                        .fillMaxWidth()
+                )
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(10.dp)
+                    .height(55.dp),
+                shape = MaterialTheme.shapes.extraSmall,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue
+                )
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = null
+                )
+                Text(text = "Guardar")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun RegistroPreview() {
     RegistroPersonaTheme {
-        Registro()
+        CamposRegistro()
     }
 }
